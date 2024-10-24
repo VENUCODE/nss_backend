c9a8dcb011c3efa3aab9c04ab3874e445e0e79df
venu <yedukondalunaik12345@gmail.com>
2024-10-24 16:48:14 +0530

    added the file uploader in the event route

--- public/index.php
+++ public/index.php
@@ -2,28 +2,40 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as Handler;
+
 use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Exception\HttpNotFoundException;
+
+// use DI\ContainerBuilder;
+// use Psr\Http\Message\UploadedFileInterface;
+
 use Slim\Factory\AppFactory;
 
 
 require "../vendor/autoload.php";
+
+// $containerBuilder = new ContainerBuilder();
+// $container = $containerBuilder->build();
+// $container->set('upload_directory', __DIR__ . '../uploads');
+
+// AppFactory::setContainer($container);
+
 $app = AppFactory::create();
+
+
 // config ,routes include section
 require "../config/db.php";
-
-
 
 //middleware adding section
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 
-//  CORS Middleware
-$app->add(function (Request $request, Handler $handler): Response {
+$app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 $beforeMiddleware = function (Request $request, Handler $handler) use ($app) {
     // Example: Check for a specific header before proceeding
@@ -42,9 +54,24 @@ $beforeMiddleware = function (Request $request, Handler $handler) use ($app) {
 
 //FIXME - change true to false for first parameter
 $app->addErrorMiddleware(true, true, true);
+$app->get('/uploads/{file}', function ($request, $response, $args) {
+    $file = '../uploads/' . $args['file'];
+    if (is_file($file)) {
+        return $response->withHeader('Content-Type', mime_content_type($file))
+            ->withHeader('Content-Disposition', 'attachment; filename="' . basename($file) . '"')
+            ->withBody(new \Slim\Psr7\Stream(fopen($file, 'r')));
+    } else {
+        return $response->withStatus(404);
+    }
+});
 // $app->add($beforeMiddleware);
 require "../routes/api.php";
 require "../routes/events.php";
 
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+    $response = $response->withStatus(404);
+    $response->getBody()->write(json_encode(['message'=>'Route not Found']));
+    return $response->withHeader('Content-Type','Application/json');
+});
 
 $app->run();
--- routes/events.php
+++ routes/events.php
@@ -2,12 +2,17 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
+function moveUploadedFile($directory, $uploadedFile) {
+    $filename = sprintf('%s.%s', bin2hex(random_bytes(8)), pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION));
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+    return $filename;
+}
 $app->group('/events', function (RouteCollectorProxy $group) {
     $group->get('/event[/{event_id}]', function (Request $request, Response $response, $args) {
         $eventId = $args['event_id'] ?? null;
         // If event_id is provided, fetch the specific event; otherwise, fetch all events
         if ($eventId) {
-            $sql = "SELECT * FROM events WHERE event_id = :event_id";
+            $sql = "SELECT * FROM (select event_id,ec.ec_name,e.ec_id,event_name,hosted_on,location from events e inner join event_category  ec on e.ec_id=ec.ec_id) res WHERE res.event_id = :event_id";
             try {
                 $database = new db();
                 $database = $database->connect();
@@ -28,7 +33,8 @@ $app->group('/events', function (RouteCollectorProxy $group) {
             }
         } else {
             // Fetch all events if event_id is not provided
-            $sql = "SELECT * FROM events";
+            $sql = "SELECT * FROM (select event_id,ec.ec_name,e.ec_id,event_name,hosted_on,location from events e inner join event_category  ec on e.ec_id=ec.ec_id) res ";
+           
             try {
                 $database = new db();
                 $database = $database->connect();
@@ -45,45 +51,71 @@ $app->group('/events', function (RouteCollectorProxy $group) {
         }
     })->setName('event');
 
-    $group->get('/category[/{category_id}]', function (Request $request, Response $response, $args) {
-        $categoryId = $args['category_id'] ?? null;
+    $group->get('/category/{category_id}', function (Request $request, Response $response, $args) {
+        $categoryId = $args['category_id'];
+        $sql = "SELECT * FROM (select event_id,ec.ec_name,e.ec_id,event_name,hosted_on,location from events e inner join event_category  ec on e.ec_id=ec.ec_id) res WHERE res.ec_id= :category_id";
+        try {
+            $database = new db();
+            $database = $database->connect();
+            $stmt = $database->prepare($sql);
+            $stmt->bindParam(':category_id', $categoryId);
+            $stmt->execute();
 
-        if ($categoryId) {
-            $sql = "SELECT * FROM events WHERE ec_id = :category_id";
-            try {
-                $database = new db();
-                $database = $database->connect();
-                $stmt = $database->prepare($sql);
-                $stmt->bindParam(':category_id', $categoryId);
-                $stmt->execute();
-
-                $events = $stmt->fetchAll(PDO::FETCH_OBJ);
-                if ($events) {
-                    $response->getBody()->write(json_encode($events));
-                } else {
-                    $response->getBody()->write(json_encode(['message' => 'No events found for this category']));
-                }
-                return $response->withStatus(200)->withHeader("Content-Type", "application/json");
-            } catch (PDOException $e) {
-                $response->getBody()->write(json_encode(['error' => ['text' => $e->getMessage()]]));
-                return $response->withStatus(500)->withHeader("Content-Type", "application/json");
-            }
-        } else {
-           $sql = "SELECT *  FROM events order by hosted_on desc";
-try {
-                $database = new db();
-                $database = $database->connect();
-                $stmt = $database->prepare($sql);
-                $stmt->execute();
-
-                $events = $stmt->fetchAll(PDO::FETCH_OBJ);
-               
+            $events = $stmt->fetchAll(PDO::FETCH_OBJ);
+            if ($events) {
                 $response->getBody()->write(json_encode($events));
-                return $response->withStatus(200)->withHeader("Content-Type", "application/json");
-            } catch (PDOException $e) {
-                $response->getBody()->write(json_encode(['error' => ['text' => $e->getMessage()]]));
-                return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+            } else {
+                $response->getBody()->write(json_encode(['message' => 'No events found for this category']));
             }
+            return $response->withStatus(200)->withHeader("Content-Type", "application/json");
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(['error' => ['text' => $e->getMessage()]]));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
         }
+    
     })->setName('category');
+    $group->post('/upload', function (Request $request, Response $response) {
+        $files = $request->getUploadedFiles();
+        $uploadDirectory = __DIR__ . '/uploads/';
+    
+        // Ensure the directory exists
+        if (!is_dir($uploadDirectory)) {
+            mkdir($uploadDirectory, 0777, true);  // Create directory with write permissions
+        }
+    
+        if (!array_key_exists('file', $files)) {
+            $response->getBody()->write(json_encode("File does not exist with name file"));
+            return $response->withHeader("Content-Type", "application/json")->withStatus(422);
+        }
+    
+        $file = $files['file'];
+        $error = $file->getError();
+        $type = $file->getClientMediaType();
+    
+        if ($error!=0) {
+            $response->getBody()->write(json_encode(['error' => 'Upload failed due to server error', 'errorcode' => $error]));
+            return $response->withStatus(422)->withHeader("Content-Type", "application/json");
+        }
+    
+        // Example of additional checks (commented)
+        $allowedTypes = ['image/png', 'image/jpeg'];
+        if (!in_array($type, $allowedTypes)) {
+            $response->getBody()->write(json_encode(['error' => 'File type not allowed', 'type' => $type]));
+            return $response->withStatus(422)->withHeader("Content-Type", "application/json");
+        }
+        if ($file->getSize() > 3000000) {
+            $response->getBody()->write(json_encode(['error' => 'File size exceeds the limit of 3MB']));
+            return $response->withStatus(422)->withHeader("Content-Type", "application/json");
+        }
+        try {
+            $filename = moveUploadedFile($uploadDirectory, $file);
+            $response->getBody()->write(json_encode(['filename' =>$uploadDirectory.$filename]));
+            return $response->withStatus(201)->withHeader("Content-Type", "application/json");
+        } catch (Throwable $th) {
+            $response->getBody()->write(json_encode(['error' => 'File could not be uploaded']));
+            return $response->withStatus(500)->withHeader("Content-Type", "application/json");
+        }
+    })->setName('upload');
+
+
 });
